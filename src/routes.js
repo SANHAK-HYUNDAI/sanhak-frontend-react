f2b0cf3d498b4ff3da726dfd9a4c6f2b230b330b
Chaerim Park <dasapcr@gmail.com>
2023-01-07 19:31:41 +0900

    0107

--- src/routes.js
+++ src/routes.js
@@ -4,7 +4,7 @@ import DashboardLayout from './layouts/dashboard';
 import LogoOnlyLayout from './layouts/LogoOnlyLayout';
 //
 import RoData from './pages/RoData';
-import Search from './pages/SearchInfo';
+import Search from './pages/Search';
 import Login from './pages/Login';
 import NotFound from './pages/Page404';
 import Register from './pages/Register';
