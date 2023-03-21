ca08cb9a6244adca7292fdbd152c4d329dc26446
Chaerim Park <dasapcr@gmail.com>
2023-03-21 18:49:58 +0900

    Modify upload.js

--- src/sections/@dashboard/products/UploadGrid.js
+++ src/sections/@dashboard/products/UploadGrid.js
@@ -2,11 +2,6 @@
 import { styled } from '@mui/material/styles';
 import { Grid, Paper, Typography } from '@mui/material';
 // components
-import cafeImage from "../../../images/GuideIcon/tea.png";
-import repair from "../../../images/GuideIcon/repair.png";
-import search from "../../../images/GuideIcon/search.png";
-import home from "../../../images/GuideIcon/home.png";
-import MakeTable from './VehicleTable';
 import './index.css';
 // components
 // ----------------------------------------------------------------------
@@ -26,11 +21,8 @@ export default function Upload() {
       <Grid item xs={12}>
         <Item elevation={4}>
           <Typography variant="h5" sx = {{ margin : 2 }}gutterBottom>
-            Types of vehicles (현대트랜시스 납부 차종 종류)
+            File upload
           </Typography>
-          <div>
-            <MakeTable />
-          </div>
         </Item>
       </Grid>
   );
